f3b09927833004a58449857899bca88a3a99488c
noriapi <70106808+noriapi@users.noreply.github.com>
2023-09-24 11:17:34 +0900

    feat(view): ネクストの向きをランダム化 (#32)
    close #21

--- src/view.ts
+++ src/view.ts
@@ -78,7 +78,7 @@ export const parseBoard = (s: string, defaultCell: AbstractCell) => {
   return board;
 };
 
-const shuffle = (array: any[]) => {
+export const shuffle = (array: any[]) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
